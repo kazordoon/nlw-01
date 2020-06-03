bda037d4dfab4e73881cbac3e089645ff1848ce6
Felipe Barros <felipetech@tuta.io>
2020-06-03 19:18:18 -0300

    chore(server): Add route to create a new point

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import knex from './database/connection';
 import Item from './contracts/Item';
+import Point from './contracts/Point';
 
 const routes = Router();
 
@@ -16,6 +17,47 @@ routes.get('/items', async (req, res) => {
     return res.json(serializedItems);
   } catch (err) {
     return res.status(500).json({ error: "Couldn't list the items." });
+  }
+});
+
+routes.post('/points', async (req, res) => {
+  try {
+    const {
+      name,
+      email,
+      whatsapp,
+      latitude,
+      longitude,
+      city,
+      uf,
+      items,
+    } = req.body;
+
+    const trx = await knex.transaction();
+
+    const [pointId] = await trx<Point>('points')
+      .insert({
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        image: 'fake_image',
+      }).returning('id');
+
+    const pointItems = items.map((itemId: number) => ({
+      item_id: itemId,
+      point_id: pointId,
+    }));
+
+    await trx('point_items')
+      .insert(pointItems);
+
+    return res.sendStatus(201);
+  } catch (err) {
+    return res.status(400).json({ error: "Couldn't create a new point." });
   }
 });
 

49020fb205de2a2433d97ef9222811133b14e4c6
Felipe Barros <felipetech@tuta.io>
2020-06-03 14:16:50 -0300

    chore(server): Improve code style

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,12 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
-app.set('PORT', process.env.PORT || 3333)
+app.set('PORT', process.env.PORT || 3333);
 
-app.get('/', (req, res) => (res.json({ hello: 'world' })))
+app.get('/', (req, res) => (res.json({ hello: 'world' })));
 
 app.listen(app.get('PORT'), () => {
-  console.log(`Server running on *:${app.get('PORT')}`)
-})
+  // eslint-disable-next-line no-console
+  console.log(`Server running on *:${app.get('PORT')}`);
+});

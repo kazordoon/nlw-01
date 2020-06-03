4411bd4f1af68f03f98c99a2b374bd47734861fc
Felipe Barros <felipetech@tuta.io>
2020-06-03 17:40:04 -0300

    chore(server): Add route to list the items

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,21 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
+import knex from './database/connection';
+import Item from './contracts/Item';
 
 const routes = Router();
 
-routes.get('/', (req: Request, res: Response) => (
-  res.json({ hello: 'world' })
-));
+routes.get('/items', async (req, res) => {
+  try {
+    const items = await knex<Item>('items').select('*');
+
+    const serializedItems = items.map((item) => ({
+      title: item.title,
+      image_url: `http://${process.env.HOST}:${process.env.PORT}/uploads/${item.image}`,
+    }));
+    return res.json(serializedItems);
+  } catch (err) {
+    return res.status(500).json({ error: "Couldn't list the items." });
+  }
+});
 
 export default routes;

714b823785c2e868f357c9ea10e4abf177e67c91
Felipe Barros <felipetech@tuta.io>
2020-06-03 19:17:46 -0300

    chore(server): Add Point interface

---
+++ server/src/contracts/Point.ts
@@ -0,0 +1,12 @@
+/* eslint-disable semi */
+export default interface Point {
+  id: number;
+  name: string;
+  image: string;
+  email: string;
+  whatsapp: string;
+  latitude: string;
+  longitude: string;
+  city: string;
+  uf: string;
+}

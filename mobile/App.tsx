1487aca360246020602dd37440bcd2c470938c8e
Felipe Barros <felipetech@tuta.io>
2020-06-06 14:56:13 -0300

    (mobile): Initial commit

---
+++ mobile/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.helloWorld}>Hello World!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  helloWorld: {
+    fontWeight: '700',
+    fontSize: 25,
+    textDecorationLine: 'underline',
+  },
+});
